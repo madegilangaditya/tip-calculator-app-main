fc9c0a706f5090d429b165ffc61660c709c262f5
gilang aditya <madegilangaditya32@gmail.com>
2023-05-05 14:17:29 +0800

    create function to add active class to tip button

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-console.log('masuk')
+console.log('masuk');
+
+const bill = document.querySelector('#bill');
+const people = document.querySelector('#people');
+const tipBtn = document.querySelectorAll('.select-tip-wrap button');
+const customTip = document.querySelector('#custom-tip');
+
+tipBtn.forEach((item) =>{
+    // console.log(item);
+    item.addEventListener('click', (e) => {
+        item.parentElement.querySelectorAll('button').forEach((ite) => {
+            removeActive(ite);
+        })
+        console.log(item.parentElement);
+        
+        tipBtnClick(e, item);
+    });
+})
+
+function tipBtnClick(e, item){
+    e.preventDefault();
+    console.log(e.target);
+   
+    e.target.classList.add('active');
+}
+
+function removeActive(item){
+    // console.log(item.parent);
+    item.classList.remove('active');
+}
